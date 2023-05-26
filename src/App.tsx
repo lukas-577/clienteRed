71dfc4929871759bac12eaebabf9c1b2a6303cbe
lukas <lukas_skyry_14@hotmail.cl>
2023-05-26 18:32:31 -0400

    primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { ListBus } from './components/ListBus';
+import { Button, TextField } from '@mui/material';
+import Container from '@mui/material/Container';
+
+
+
+export interface IBusData {
+  bus: string,
+  seDirige: string,
+  DesvioPlanificado: string,
+  tiempoEstimado: string
+}
+
 
 function App() {
+
+
+  const [busList, setBusList] = useState<IBusData[]>([]);
+  const [input, setImput] = useState('');
+
+
+
+
+
+
+  const getBus = async () => {
+    try {
+      const respuesta = await fetch(`http://localhost:5000/datos/pa1`);
+      const datosJson = await respuesta.json();
+      const datos = datosJson.map((x: any) => x.bus);
+      console.log(datos);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getBusThen = async () => {
+    try {
+      await fetch(`http://localhost:5000/datos/pa1`)
+        .then((res) => res.json())
+        .then((datos) => datos.map((x: any) => console.log(x.bus)));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  // const getBusInterface = async () => {
+  //   try {
+  //     await fetch(`http://localhost:5000/datos/pa1`)
+  //       .then((res) => res.json())
+  //       .then((datos) => {
+  //         console.log(datos);
+  //         datos.forEach((res: IBusData[]) => { //recorro todos los datos bus y cambio el estado
+  //           const bus = res as IBusData[];
+  //           setBusList(bus);
+  //           console.log(setBusList(bus));
+  //           console.log(bus);
+  //         })
+
+  //       });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+
+  const getBusInterface2 = async () => {
+    try {
+      await fetch(`http://localhost:5000/datos/${input}`)
+        .then((res) => res.json())
+        .then((datos) => {
+          console.log(datos);
+          const bus = datos as IBusData[];
+          setBusList(bus);
+          console.log(bus);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handlerChange = (value: string) => {
+    setImput(value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+
+        <header className="App-header">
+          <TextField variant="standard" color="success" value={input} onChange={(x) => handlerChange(x.target.value)} placeholder="Ingresa paradero" />
+          <p>
+            {/* <button onClick={() => { getBus() }}>boton1</button>
+            <button onClick={() => { getBusThen() }}>botonThen</button> */}
+            {/* <button onClick={() => { getBusInterface() }}>botonInterface</button> */}
+            <Button color="success" onClick={() => { getBusInterface2() }}>busca recorridos</Button>
+          </p>
+          <div>
+            {ListBus(busList)}
+          </div>
+        </header>
+      </Container>
     </div>
   );
 }
---
+++ src/components/ListBus.tsx
@@ -0,0 +1,32 @@
+import { IBusData } from "../App"
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+
+export const ListBus = (busList: IBusData[]) => {
+
+    return (
+        <>
+            <Grid container spacing={12}>
+                {busList.map((bus) => (
+                    <Grid item key={bus.bus} xs={12} sm={6} md={4} lg={4}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+                            <Paper elevation={3} style={{ borderRadius: '50px', padding: '20px', backgroundColor: "#cddc39" }}>
+                                <DirectionsBusIcon fontSize="large" />
+                                <h4>{bus.bus}</h4>
+                                <h5>{bus.seDirige}</h5>
+                                <h6>{bus.DesvioPlanificado}</h6>
+                                <h6>{bus.tiempoEstimado}</h6>
+                            </Paper>
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+
+    )
+
+
+
+}
