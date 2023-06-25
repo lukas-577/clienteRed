af8823590cf8229cc82aaf7205e71a717ce50b31
Lukas <lukas_skyry_14@hotmail.cl>
2023-06-25 15:43:54 -0400

    sol error no ingresa paradero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import { ListBus } from './components/ListBus';
+import Box from '@mui/material/Box';
 import { Button, TextField } from '@mui/material';
 import Container from '@mui/material/Container';
 import { IBusData } from './interfaces/busData.interface';
 import { Error, IProps } from './components/Error';
 import { Loading } from './components/Loading';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 
@@ -19,7 +20,31 @@ function App() {
   const [input, setImput] = useState('');
   const [error, setError] = useState({ isError: false, message: '' });
   const [loading, setLoading] = useState(false);
+  const [paraderoError, setParaderoError]= useState({error:false, message: ''});
 
+
+
+  const validarParadero=(paradero: string)=>{
+    const expresionRegular = /(^[A-Za-z]{2}\d+$)/
+    return expresionRegular.test(paradero)
+  };
+
+  const handleSubmint = (e:any)=>{
+    e.preventDefault();
+    if(!validarParadero(input)){
+      setParaderoError({
+        error: true,
+        message: 'Mal ingresado el paradero'
+      })
+    }else{
+      setParaderoError({
+        error: false,
+        message: ''
+      })
+      console.log('paradero correcto');
+      getBusInterface2();
+    }
+  }
 
   const getBusInterface2 = async () => {
     setLoading(true);
@@ -53,23 +78,39 @@ function App() {
   }
 
 
+// creacion de thema 
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000', // Color primario
+    },
+    secondary: {
+      main: '#f50057', // Color secundario
+    },
+  },
+});
+
   return (
     <div className="App">
       <Container>
-
-        <header className="App-header">
-          <TextField variant="standard" color="success" value={input} onChange={(x) => handlerChange(x.target.value)} placeholder="Ingresa paradero" />
-          <p>
-            <Button color="success" onClick={() => { getBusInterface2() }}>busca recorridos</Button>
-          </p>
-          <div>
-            {loading && (<Loading></Loading>)}
-            {!loading && error.isError && (<Error message={error.message} />)}
-            {!loading && !error.isError && (
-              ListBus(busList)
-            )}
-          </div>
-        </header>
+        <ThemeProvider theme={theme}>
+          <header className="App-header">
+            <Box component={"form"} onSubmit={handleSubmint}>
+              <TextField required helperText={paraderoError.message} error={paraderoError.error} color='primary' variant="outlined" value={input} onChange={(x) => handlerChange(x.target.value)} placeholder="Ingrese paradero" />
+              <p>
+                <Button type='submit' color='primary'>busca recorridos</Button>
+              </p>
+            </Box>
+            <div>
+              {loading && (<Loading></Loading>)}
+              {!loading && error.isError && (<Error message={error.message} />)}
+              {!loading && !error.isError && (
+                ListBus(busList)
+              )}
+            </div>
+          </header>
+        </ThemeProvider>
       </Container>
     </div>
   );
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { CircularProgress } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// creacion de thema 
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#000000', // Color primario
+      },
+      secondary: {
+        main: '#f50057', // Color secundario
+      },
+    },
+  });
 
 export const Loading = () => {
     return (
         <div>
-            <p>
-                Obteniendo Datos...
-            </p>
-            <CircularProgress color="success" />
+            <ThemeProvider theme={theme}>
+                <p>
+                    Obteniendo Datos...
+                </p>
+                <CircularProgress color="primary" />
+            </ThemeProvider>
         </div>
     )
 }
